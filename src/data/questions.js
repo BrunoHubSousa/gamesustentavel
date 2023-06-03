b0ab0b7f1a28d196f32aaaaf3731f060b4d2046b
Bruno Sousa da Silva <brunosousasilva@live.com>
2023-06-03 01:28:22 -0300

    first commit

---
+++ src/data/questions.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    question: 'O que significa o termo "desenvolvimento sustentável"?',
+    options: [
+      "Crescimento econômico sem consideração ao meio ambiente.",
+      "Desenvolvimento que visa apenas a sustentabilidade ambiental.",
+      "Desenvolvimento que não prejudica as gerações futuras.",
+      "Desenvolvimento que só considera o presente.",
+    ],
+    answer: "Desenvolvimento que não prejudica as gerações futuras.",
+  },
+  {
+    question: "Qual das seguintes energias é renovável?",
+    options: ["Carvão", "Petróleo", "Gás Natural", "Energia Solar"],
+    answer: "Energia Solar",
+  },
+  {
+    question: "O que é a pegada ecológica?",
+    options: [
+      "É o tamanho físico do nosso pé.",
+      "É a marca deixada pelos animais na floresta.",
+      "É a medida do impacto humano no meio ambiente.",
+      "É o número de árvores plantadas por uma pessoa.",
+    ],
+    answer: "É a medida do impacto humano no meio ambiente.",
+  },
+  {
+    question: "Qual destes é considerado um dos 3 R's da sustentabilidade?",
+    options: ["Reagir", "Reciclar", "Relatar", "Revisar"],
+    answer: "Reciclar",
+  },
+  {
+    question:
+      "O que é a Conferência das Nações Unidas sobre Desenvolvimento Sustentável (Rio+20)?",
+    options: [
+      "Um evento sobre a economia brasileira.",
+      "Uma conferência sobre segurança global.",
+      "Uma competição internacional de futebol.",
+      "Uma reunião para discutir o futuro do meio ambiente e o desenvolvimento sustentável.",
+    ],
+    answer:
+      "Uma reunião para discutir o futuro do meio ambiente e o desenvolvimento sustentável.",
+  },
+];
+
+export default data;
